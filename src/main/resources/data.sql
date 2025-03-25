1538e5051fcc6ddd6e91794332ce80c5b2c61dcc
chuksabadom <chuksabadom@gmail.com>
2025-03-25 08:16:59 +0100

    Add more customer data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,5 @@
 INSERT INTO customer (ID, NAME, SURNAME) VALUES ('550e8400-e29b-41d4-a716-446655440000', 'Alice', 'Smith');
+INSERT INTO customer (ID, NAME, SURNAME) VALUES ('33637e0d-4bd0-4a38-a22c-2067f94b4e79', 'Emeka', 'Abadom');
+INSERT INTO customer (ID, NAME, SURNAME) VALUES ('b3c7914c-c89b-42e1-8345-3c6c595a480f', 'Chuks', 'Chief');
+INSERT INTO customer (ID, NAME, SURNAME) VALUES ('2175c290-20d5-46ed-8ba2-054fbd1aea52', 'Enene', 'Cole');
+INSERT INTO customer (ID, NAME, SURNAME) VALUES ('bd53ba6a-9aa6-4527-9ef5-6939053b13b7', 'Muna', 'Chizi');
